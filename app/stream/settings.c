8977805de42e8c895a2751db910bf3929002f351
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-01-05 18:12:19 +0900

    Code cleanup

--- app/stream/settings.c
+++ app/stream/settings.c
@@ -8,9 +8,6 @@
 
 #include <sys/stat.h>
 
-static const char conf_dir[] = ".moonlight-sdl";
-static const char conf_name_streaming[] = "streaming.conf";
-
 static char *_path_join(const char *parent, const char *basename)
 {
     char *joined = calloc(strlen(parent) + strlen(basename), sizeof(char));
@@ -21,7 +18,7 @@ static char *_path_join(const char *parent, const char *basename)
 static char *settings_config_dir()
 {
     char *homedir = getenv("HOME");
-    char *confdir = _path_join(homedir, conf_dir);
+    char *confdir = _path_join(homedir, CONF_DIR);
     if (access(confdir, F_OK) == -1)
     {
         if (errno == ENOENT)
@@ -36,7 +33,7 @@ PCONFIGURATION settings_load()
 {
     PCONFIGURATION config = malloc(sizeof(CONFIGURATION));
     char *confdir = settings_config_dir();
-    char *argv[2] = {"moonlight", _path_join(confdir, conf_name_streaming)};
+    char *argv[2] = {"moonlight", _path_join(confdir, CONF_NAME_STREAMING)};
     config_parse(2, argv, config);
     free(argv[1]);
 
--- app/stream/settings.h
+++ app/stream/settings.h
@@ -1,4 +1,7 @@
 #pragma once
 #include "config.h"
 
+#define CONF_DIR ".moonlight-sdl"
+#define CONF_NAME_STREAMING "streaming.conf"
+
 PCONFIGURATION settings_load();
