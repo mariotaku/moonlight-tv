24a98230742c357c61628400f5473a4aabc3d8f7
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-01-10 19:01:06 +0900

    TV UI begin

--- app/main.c
+++ app/main.c
@@ -57,15 +57,10 @@ int main(int argc, char *argv[])
     {
         struct nk_font_atlas *atlas;
         nk_platform_font_stash_begin(&atlas);
-        /*struct nk_font *droid = nk_font_atlas_add_from_file(atlas, "../../../extra_font/DroidSans.ttf", 14, 0);*/
-        /*struct nk_font *roboto = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Roboto-Regular.ttf", 16, 0);*/
-        /*struct nk_font *future = nk_font_atlas_add_from_file(atlas, "../../../extra_font/kenvector_future_thin.ttf", 13, 0);*/
-        /*struct nk_font *clean = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyClean.ttf", 12, 0);*/
-        /*struct nk_font *tiny = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyTiny.ttf", 10, 0);*/
-        /*struct nk_font *cousine = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Cousine-Regular.ttf", 13, 0);*/
+        struct nk_font *noto = nk_font_atlas_add_from_file(atlas, "assets/NotoSans-Regular.ttf", 20, NULL);
         nk_platform_font_stash_end();
         /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
-        /*nk_style_set_font(ctx, &roboto->handle)*/;
+        nk_style_set_font(ctx, &noto->handle);
     }
 
     /* style.c */
--- app/stream/input/sdl.h
+++ app/stream/input/sdl.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <stdbool.h>
+
 #include <SDL.h>
 
 bool absinput_dispatch_event(SDL_Event ev);
