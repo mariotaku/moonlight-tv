4089cd39cb9f5d7f0c5f895cb14215a7971b9f79
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-01-12 23:30:22 +0900

    Finished stb_image implementation

--- app/platform/lgnc/nkimage.c
+++ app/platform/lgnc/nkimage.c
@@ -7,37 +7,71 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+struct stb_image
+{
+    int x, y, n;
+    unsigned char *data;
+};
+
 NK_API nk_bool nk_loadimage(const char *path, struct nk_image *img)
 {
     int x, y, n;
-    unsigned char *data = stbi_load(path, &x, &y, &n, 0);
-    if (!data)
+    struct stb_image *s = malloc(sizeof(struct stb_image));
+    memset(s, 0, sizeof(struct stb_image));
+    s->data = stbi_load(path, &x, &y, &n, 0);
+    if (!s->data)
     {
+        free(s);
         return nk_false;
     }
+    s->x = x;
+    s->y = y;
+    s->n = n;
+
     img->w = x;
     img->h = y;
     img->region[0] = 0;
     img->region[1] = 0;
     img->region[2] = x;
     img->region[3] = y;
-    img->handle.ptr = data;
+    img->handle.ptr = s;
     return nk_true;
 }
 
 NK_API nk_bool nk_conv2gl(struct nk_image *img)
 {
-    unsigned char *data = img->handle.ptr;
+    struct stb_image *s = img->handle.ptr;
     GLuint tex;
+    GLenum texfmt;
+
+    if (s->n == 4)
+    {
+        texfmt = GL_RGBA;
+    }
+    else if (s->n == 3)
+    {
+        texfmt = GL_RGB;
+    }
+    else
+    {
+        stbi_image_free(s->data);
+        free(s);
+        return nk_false;
+    }
+
     glGenTextures(1, &tex);
     glBindTexture(GL_TEXTURE_2D, tex);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->w, img->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, texfmt, img->w, img->h, 0, texfmt, GL_UNSIGNED_BYTE, s->data);
     glGenerateMipmap(GL_TEXTURE_2D);
-    stbi_image_free(data);
+
+    img->handle.id = tex;
+
+    stbi_image_free(s->data);
+    free(s);
     return nk_true;
 }
 
