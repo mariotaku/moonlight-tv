43a84725c45880dcf575dc15cdc593e336deb559
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-01-22 16:23:41 +0900

    Updated gamecontorllerdb for webOS

--- scripts/webos/gen_gamecontrollerdb.sh
+++ scripts/webos/gen_gamecontrollerdb.sh
@@ -5,5 +5,6 @@ if [ ! -d assets ]; then
 fi
 
 echo "# Generated from https://github.com/gabomdq/SDL_GameControllerDB" > assets/gamecontrollerdb.txt
+git -C third_party/SDL_GameControllerDB log -1 --pretty=format:"# %h %ad - %s%n" >> assets/gamecontrollerdb.txt
 echo >> assets/gamecontrollerdb.txt
 grep ',platform:Linux,$' third_party/SDL_GameControllerDB/gamecontrollerdb.txt | sed 's/,platform:Linux,$/,platform:webOS,/' >> assets/gamecontrollerdb.txt
