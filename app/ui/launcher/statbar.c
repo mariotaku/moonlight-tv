2e5b258cb126a2349ed5f1dc0d0e8a74b26924c5
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-01-31 03:45:38 +0900

    Statbar when not paired

--- app/ui/launcher/statbar.c
+++ app/ui/launcher/statbar.c
@@ -58,7 +58,7 @@ static inline struct nk_image ic_navkey_confirm()
 
 void launcher_statbar(struct nk_context *ctx)
 {
-    bool game_selected = applist_hovered_item != NULL || ui_input_mode == UI_INPUT_MODE_POINTER;
+    bool apps_showing = selected_server_node && selected_server_node->server;
     bool is_running = applist_hovered_item && selected_server_node && selected_server_node->server &&
                       applist_hovered_item->id == selected_server_node->server->currentGame;
 
@@ -77,7 +77,7 @@ void launcher_statbar(struct nk_context *ctx)
         nk_layout_row_template_push_static_s(ctx, launcher_bottom_bar_height_dp);
         nk_layout_row_template_push_static(ctx, nk_string_measure_width(ctx, "Confirm"));
     }
-    else
+    else if (apps_showing)
     {
         nk_layout_row_template_push_static_s(ctx, launcher_bottom_bar_height_dp);
         nk_layout_row_template_push_static(ctx, nk_string_measure_width(ctx, "Change PC"));
@@ -90,6 +90,11 @@ void launcher_statbar(struct nk_context *ctx)
         nk_layout_row_template_push_static_s(ctx, 1);
         nk_layout_row_template_push_static_s(ctx, launcher_bottom_bar_height_dp);
         nk_layout_row_template_push_static(ctx, nk_string_measure_width(ctx, is_running ? STR_ACTION_RESUME : STR_ACTION_LAUNCH));
+    }
+    else
+    {
+        nk_layout_row_template_push_static_s(ctx, launcher_bottom_bar_height_dp);
+        nk_layout_row_template_push_static(ctx, nk_string_measure_width(ctx, "Select PC"));
     }
     nk_layout_row_template_end(ctx);
 
@@ -107,7 +112,7 @@ void launcher_statbar(struct nk_context *ctx)
         nk_image_padded(ctx, ic_navkey_confirm(), statbar_image_padding);
         nk_label(ctx, "Confirm", NK_TEXT_RIGHT);
     }
-    else
+    else if (apps_showing)
     {
         nk_image_padded(ctx, ic_navkey_menu(), statbar_image_padding);
         nk_label(ctx, "Change PC", NK_TEXT_RIGHT);
@@ -120,5 +125,10 @@ void launcher_statbar(struct nk_context *ctx)
         nk_spacing(ctx, 1);
         nk_image_padded(ctx, ic_navkey_confirm(), statbar_image_padding);
         nk_label(ctx, is_running ? STR_ACTION_RESUME : STR_ACTION_LAUNCH, NK_TEXT_RIGHT);
+    }
+    else
+    {
+        nk_image_padded(ctx, ic_navkey_menu(), statbar_image_padding);
+        nk_label(ctx, "Select PC", NK_TEXT_RIGHT);
     }
 }
