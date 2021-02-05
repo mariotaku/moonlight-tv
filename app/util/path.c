70c90b5be726d06fe8d76a8fc290e8fbb0e87a97
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-02-06 00:01:01 +0900

    Improved path join

--- app/util/path.c
+++ app/util/path.c
@@ -6,7 +6,12 @@
 
 char *path_join(const char *parent, const char *basename)
 {
-    char *joined = calloc(strlen(parent) + 1 + strlen(basename) + 1, sizeof(char));
-    sprintf(joined, "%s/%s", parent, basename);
+    int parentlen = strlen(parent);
+    if (parentlen && parent[parentlen - 1] == '/')
+    {
+        parentlen -= 1;
+    }
+    char *joined = calloc(parentlen + 1 + strlen(basename) + 1, sizeof(char));
+    sprintf(joined, "%.*s/%s", parentlen, parent, basename);
     return joined;
 }
