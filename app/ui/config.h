174a7290a65c61e94caa4c26b6984f9ae0fe8f34
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-02-06 22:36:38 +0000

    Big screen for Pi

--- app/ui/config.h
+++ app/ui/config.h
@@ -1,15 +1,9 @@
 #pragma once
 
-#ifdef OS_WEBOS
-#ifndef WEBOS_LEGACY
+#if OS_WEBOS || TARGET_RASPI
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 #define NK_UI_SCALE 3
-#else
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
-#define NK_UI_SCALE 2
-#endif
 #else
 #define WINDOW_WIDTH 960
 #define WINDOW_HEIGHT 540
