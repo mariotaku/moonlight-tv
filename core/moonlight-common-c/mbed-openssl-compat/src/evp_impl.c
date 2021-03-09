0afcc125a8a171882b8de4f06b2ba7a1388b4735
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-03-10 02:00:53 +0900

    mbedtls session encryption works

--- core/moonlight-common-c/mbed-openssl-compat/src/evp_impl.c
+++ core/moonlight-common-c/mbed-openssl-compat/src/evp_impl.c
@@ -10,9 +10,14 @@
 
 struct EVP_CIPHER_CTX_T
 {
-    mbedtls_gcm_context ctx;
     mbedtls_cipher_type_t cipher;
+    unsigned char iv[16];
     int ivlen;
+    union
+    {
+        mbedtls_gcm_context gcm;
+        mbedtls_aes_context aes;
+    } ctx;
 };
 
 static int failed_logging(int ret);
@@ -22,7 +27,6 @@ EVP_CIPHER_CTX *mbed_EVP_CIPHER_CTX_new(void)
     EVP_CIPHER_CTX *ctx = malloc(sizeof(EVP_CIPHER_CTX));
     ctx->cipher = MBEDTLS_CIPHER_NONE;
     ctx->ivlen = 0;
-    mbedtls_gcm_init(&ctx->ctx);
     return ctx;
 }
 
@@ -31,7 +35,7 @@ int mbed_EVP_CIPHER_CTX_ctrl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr)
     switch (type)
     {
     case EVP_CTRL_GCM_GET_TAG:
-        return mbedtls_gcm_finish(&ctx->ctx, (unsigned char *)ptr, arg) == 0 ? 1 : 0;
+        return mbedtls_gcm_finish(&ctx->ctx.gcm, (unsigned char *)ptr, arg) == 0 ? 1 : 0;
     case EVP_CTRL_GCM_SET_IVLEN:
         ctx->ivlen = arg;
         return 1;
@@ -46,16 +50,34 @@ int mbed_EVP_EncryptInit_ex(EVP_CIPHER_CTX *ctx,
                             const unsigned char *key,
                             const unsigned char *iv)
 {
-    if (!key || !iv)
+    if (cipher)
     {
         ctx->cipher = cipher;
+    }
+    if (!key || !iv)
+    {
         return 1;
     }
     int ret;
-    if ((ret = mbedtls_gcm_setkey(&ctx->ctx, MBEDTLS_CIPHER_ID_AES, key, 128)) != 0)
-        return failed_logging(ret);
-    if ((ret = mbedtls_gcm_starts(&ctx->ctx, MBEDTLS_GCM_ENCRYPT, iv, ctx->ivlen, NULL, 0)) != 0)
-        return failed_logging(ret);
+
+    switch (ctx->cipher)
+    {
+    case MBEDTLS_CIPHER_AES_128_GCM:
+        mbedtls_gcm_init(&ctx->ctx.gcm);
+        if ((ret = mbedtls_gcm_setkey(&ctx->ctx.gcm, MBEDTLS_CIPHER_ID_AES, key, 128)) != 0)
+            return failed_logging(ret);
+        memcpy(ctx->iv, iv, ctx->ivlen);
+        if ((ret = mbedtls_gcm_starts(&ctx->ctx.gcm, MBEDTLS_GCM_ENCRYPT, iv, ctx->ivlen, NULL, 0)) != 0)
+            return failed_logging(ret);
+        break;
+    case MBEDTLS_CIPHER_AES_128_CBC:
+        mbedtls_aes_init(&ctx->ctx.aes);
+        if ((ret = mbedtls_aes_setkey_enc(&ctx->ctx.aes, key, 128)) != 0)
+            return failed_logging(ret);
+        memcpy(ctx->iv, iv, 16);
+        break;
+    }
+
     return 1;
 }
 
@@ -63,9 +85,19 @@ int mbed_EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx, unsigned char *out,
                            int *outl, const unsigned char *in, int inl)
 {
     int ret;
-    if ((ret = mbedtls_gcm_update(&ctx->ctx, inl, in, out)) != 0)
-        return failed_logging(ret);
-    *outl = 16;
+
+    switch (ctx->cipher)
+    {
+    case MBEDTLS_CIPHER_AES_128_GCM:
+        if ((ret = mbedtls_gcm_update(&ctx->ctx.gcm, inl, in, out)) != 0)
+            return failed_logging(ret);
+        break;
+    case MBEDTLS_CIPHER_AES_128_CBC:
+        if ((ret = mbedtls_aes_crypt_cbc(&ctx->ctx.aes, MBEDTLS_AES_ENCRYPT, inl, ctx->iv, in, out)) != 0)
+            return failed_logging(ret);
+        break;
+    }
+    *outl = inl;
     return 1;
 }
 
@@ -78,14 +110,31 @@ int mbed_EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx, unsigned char *out,
 
 int mbed_EVP_CIPHER_CTX_reset(EVP_CIPHER_CTX *c)
 {
-    mbedtls_gcm_free(&c->ctx);
-    mbedtls_gcm_init(&c->ctx);
+    switch (c->cipher)
+    {
+    case MBEDTLS_CIPHER_AES_128_GCM:
+        mbedtls_gcm_free(&c->ctx.gcm);
+        mbedtls_gcm_init(&c->ctx.gcm);
+        break;
+    case MBEDTLS_CIPHER_AES_128_CBC:
+        mbedtls_aes_free(&c->ctx.aes);
+        mbedtls_aes_init(&c->ctx.aes);
+        break;
+    }
     return 1;
 }
 
 void mbed_EVP_CIPHER_CTX_free(EVP_CIPHER_CTX *c)
 {
-    mbedtls_gcm_free(&c->ctx);
+    switch (c->cipher)
+    {
+    case MBEDTLS_CIPHER_AES_128_GCM:
+        mbedtls_gcm_free(&c->ctx.gcm);
+        break;
+    case MBEDTLS_CIPHER_AES_128_CBC:
+        mbedtls_aes_free(&c->ctx.aes);
+        break;
+    }
     free(c);
 }
 
--- core/moonlight-common-c/mbed-openssl-compat/test/evp.c
+++ core/moonlight-common-c/mbed-openssl-compat/test/evp.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 #include <assert.h>
 #include <memory.h>
+#include <stdbool.h>
 
 #include "evp_impl.h"
+#include <openssl/rand.h>
 #include <openssl/evp.h>
+
+#define MAX_INPUT_PACKET_SIZE 128
+
+#define ROUND_TO_PKCS7_PADDED_LEN(x) ((((x) + 15) / 16) * 16)
 
 const static unsigned char aesKey[16] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
 const static unsigned char aesIv[16] = {0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1};
 
-int do_compat(const unsigned char *plaintext, int plaintextLen,
-              unsigned char *ciphertext, int *ciphertextLen);
-int do_native(const unsigned char *plaintext, int plaintextLen,
-              unsigned char *ciphertext, int *ciphertextLen);
+int do_compat_gcm(const unsigned char *plaintext, int plaintextLen,
+                  unsigned char *ciphertext, int *ciphertextLen);
+int do_native_gcm(const unsigned char *plaintext, int plaintextLen,
+                  unsigned char *ciphertext, int *ciphertextLen);
+int do_compat_cbc(const unsigned char *plaintext, int plaintextLen,
+                  unsigned char *ciphertext, int *ciphertextLen);
+int do_native_cbc(const unsigned char *plaintext, int plaintextLen,
+                  unsigned char *ciphertext, int *ciphertextLen);
+int AppVersionQuad[4];
 
 int main(int argc, char *argv[])
 {
-    const char plaintext[16] = "0123456789abcdef";
+    const char plaintext[16];
     char actual_txt[128], expected_txt[128];
     for (int i = 0; i < 10; i++)
     {
+        RAND_bytes(plaintext, 12);
         memset(actual_txt, 0, 128);
         memset(expected_txt, 0, 128);
         int actual_len = 0, expected_len = 0;
-        assert(do_native(plaintext, 16, expected_txt, &expected_len) == 0);
-        assert(do_compat(plaintext, 16, actual_txt, &actual_len) == 0);
+        if (i > 5)
+        {
+            assert(do_native_gcm(plaintext, 12, expected_txt, &expected_len) == 0);
+            assert(do_compat_gcm(plaintext, 12, actual_txt, &actual_len) == 0);
+        }
+        else
+        {
+            assert(do_native_cbc(plaintext, 12, expected_txt, &expected_len) == 0);
+            assert(do_compat_cbc(plaintext, 12, actual_txt, &actual_len) == 0);
+        }
 
         assert(actual_len == expected_len);
         assert(memcmp(actual_txt, expected_txt, actual_len) == 0);
@@ -31,13 +51,37 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-int do_compat(const unsigned char *plaintext, int plaintextLen,
+static int addPkcs7PaddingInPlace(unsigned char *plaintext, int plaintextLen)
+{
+    int i;
+    int paddedLength = ROUND_TO_PKCS7_PADDED_LEN(plaintextLen);
+    unsigned char paddingByte = (unsigned char)(16 - (plaintextLen % 16));
+
+    for (i = plaintextLen; i < paddedLength; i++)
+    {
+        plaintext[i] = paddingByte;
+    }
+
+    return paddedLength;
+}
+
+int do_compat_gcm(const unsigned char *plaintext, int plaintextLen,
               unsigned char *ciphertext, int *ciphertextLen)
 {
+    static struct EVP_CIPHER_CTX_T *cipherContext = NULL;
+    static bool cipherInitialized = false;
+
     int ret;
     int len;
 
-    struct EVP_CIPHER_CTX_T *cipherContext;
+    if (!cipherInitialized)
+    {
+        if ((cipherContext = mbed_EVP_CIPHER_CTX_new()) == NULL)
+        {
+            return -1;
+        }
+        cipherInitialized = true;
+    }
 
     if ((cipherContext = mbed_EVP_CIPHER_CTX_new()) == NULL)
     {
@@ -93,21 +137,27 @@ int do_compat(const unsigned char *plaintext, int plaintextLen,
     ret = 0;
 
 gcm_cleanup:
-    mbed_EVP_CIPHER_CTX_free(cipherContext);
+    mbed_EVP_CIPHER_CTX_reset(cipherContext);
+
     return ret;
 }
 
-int do_native(const unsigned char *plaintext, int plaintextLen,
+int do_native_gcm(const unsigned char *plaintext, int plaintextLen,
               unsigned char *ciphertext, int *ciphertextLen)
 {
+    static EVP_CIPHER_CTX *cipherContext = NULL;
+    static bool cipherInitialized = false;
+
     int ret;
     int len;
 
-    EVP_CIPHER_CTX *cipherContext;
-
-    if ((cipherContext = EVP_CIPHER_CTX_new()) == NULL)
+    if (!cipherInitialized)
     {
-        return -1;
+        if ((cipherContext = EVP_CIPHER_CTX_new()) == NULL)
+        {
+            return -1;
+        }
+        cipherInitialized = true;
     }
 
     // Gen 7 servers use 128-bit AES GCM
@@ -159,6 +209,101 @@ int do_native(const unsigned char *plaintext, int plaintextLen,
     ret = 0;
 
 gcm_cleanup:
-    EVP_CIPHER_CTX_free(cipherContext);
+    EVP_CIPHER_CTX_reset(cipherContext);
+
+    return ret;
+}
+
+int do_compat_cbc(const unsigned char *plaintext, int plaintextLen,
+                  unsigned char *ciphertext, int *ciphertextLen)
+{
+    static struct EVP_CIPHER_CTX_T *cipherContext = NULL;
+    static bool cipherInitialized = false;
+
+    int ret;
+    int len;
+
+    unsigned char paddedData[MAX_INPUT_PACKET_SIZE];
+    int paddedLength;
+
+    if (!cipherInitialized)
+    {
+        if ((cipherContext = mbed_EVP_CIPHER_CTX_new()) == NULL)
+        {
+            ret = -1;
+            goto cbc_cleanup;
+        }
+        cipherInitialized = true;
+
+        // Prior to Gen 7, 128-bit AES CBC is used for encryption
+        if (mbed_EVP_EncryptInit_ex(cipherContext, mbed_EVP_aes_128_cbc(), NULL, aesKey, aesIv) != 1)
+        {
+            ret = -1;
+            goto cbc_cleanup;
+        }
+    }
+
+    // Pad the data to the required block length
+    memcpy(paddedData, plaintext, plaintextLen);
+    paddedLength = addPkcs7PaddingInPlace(paddedData, plaintextLen);
+
+    if (mbed_EVP_EncryptUpdate(cipherContext, ciphertext, ciphertextLen, paddedData, paddedLength) != 1)
+    {
+        ret = -1;
+        goto cbc_cleanup;
+    }
+
+    ret = 0;
+
+cbc_cleanup:
+    // Nothing to do
+    ;
+    return ret;
+}
+
+int do_native_cbc(const unsigned char *plaintext, int plaintextLen,
+                  unsigned char *ciphertext, int *ciphertextLen)
+{
+    static EVP_CIPHER_CTX *cipherContext = NULL;
+    static bool cipherInitialized = false;
+
+    int ret;
+    int len;
+
+    unsigned char paddedData[MAX_INPUT_PACKET_SIZE];
+    int paddedLength;
+
+    if (!cipherInitialized)
+    {
+        if ((cipherContext = EVP_CIPHER_CTX_new()) == NULL)
+        {
+            ret = -1;
+            goto cbc_cleanup;
+        }
+        cipherInitialized = true;
+
+        // Prior to Gen 7, 128-bit AES CBC is used for encryption
+        if (EVP_EncryptInit_ex(cipherContext, EVP_aes_128_cbc(), NULL, aesKey, aesIv) != 1)
+        {
+            ret = -1;
+            goto cbc_cleanup;
+        }
+    }
+
+    // Pad the data to the required block length
+    memcpy(paddedData, plaintext, plaintextLen);
+    paddedLength = addPkcs7PaddingInPlace(paddedData, plaintextLen);
+
+    if (EVP_EncryptUpdate(cipherContext, ciphertext, ciphertextLen, paddedData, paddedLength) != 1)
+    {
+        ret = -1;
+        goto cbc_cleanup;
+    }
+
+    ret = 0;
+
+cbc_cleanup:
+    // Nothing to do
+    ;
     return ret;
 }
