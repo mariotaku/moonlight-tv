f35bce571bed0537afe2a6f3dd018da8bc00124a
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-03-12 02:27:08 +0900

    Pairing WIP

--- core/libgamestream/src/client.c
+++ core/libgamestream/src/client.c
@@ -31,6 +31,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 #include <uuid/uuid.h>
 #include <mbedtls/sha1.h>
 #include <mbedtls/sha256.h>
@@ -40,6 +41,9 @@
 #include <mbedtls/pk.h>
 #include <mbedtls/x509_crt.h>
 #include <mbedtls/rsa.h>
+#include <mbedtls/error.h>
+
+#include <assert.h>
 
 #define UNIQUE_FILE_NAME "uniqueid.dat"
 
@@ -49,9 +53,15 @@
 static char unique_id[UNIQUEID_CHARS + 1];
 static mbedtls_pk_context privateKey;
 static mbedtls_x509_crt cert;
-static char cert_hex[4096];
+static char cert_bin[4096], cert_hex[4096];
 
 const char *gs_error;
+
+static bool generateSignature(const unsigned char *msg, size_t mlen, unsigned char *sig, size_t *slen, mbedtls_pk_context *pkey, mbedtls_ctr_drbg_context *rng);
+static bool verifySignature(const unsigned char *msg, size_t mlen, const unsigned char *sig, size_t slen, mbedtls_x509_crt *cert);
+static bool construct_url(char *url, size_t ulen, bool secure, const char *address, const char *action, const char *fmt, ...);
+static int hash_data(mbedtls_md_type_t type, const unsigned char *input, size_t ilen, unsigned char *output, size_t *olen);
+static bool crypt_data(mbedtls_aes_context *ctx, int mode, const unsigned char input[16], unsigned char output[16], size_t len);
 
 static int mkdirtree(const char *directory)
 {
@@ -148,8 +158,10 @@ static int load_cert(const char *keyDirectory)
   while ((c = fgetc(fd)) != EOF)
   {
     sprintf(&cert_hex[length], "%02x", c);
+    cert_bin[length / 2] = c;
     length += 2;
   }
+  cert_bin[length / 2] = 0;
   cert_hex[length] = 0;
 
   fclose(fd);
@@ -166,10 +178,6 @@ static int load_cert(const char *keyDirectory)
 
 static int load_server_status(PSERVER_DATA server)
 {
-
-  uuid_t uuid;
-  char uuid_str[37];
-
   int ret;
   char url[4096];
   int i;
@@ -184,15 +192,11 @@ static int load_server_status(PSERVER_DATA server)
 
     ret = GS_INVALID;
 
-    uuid_generate_random(uuid);
-    uuid_unparse(uuid, uuid_str);
-
     // Modern GFE versions don't allow serverinfo to be fetched over HTTPS if the client
     // is not already paired. Since we can't pair without knowing the server version, we
     // make another request over HTTP if the HTTPS request fails. We can't just use HTTP
     // for everything because it doesn't accurately tell us if we're paired.
-    snprintf(url, sizeof(url), "%s://%s:%d/serverinfo?uniqueid=%s&uuid=%s",
-             i == 0 ? "https" : "http", server->serverInfo.address, i == 0 ? 47984 : 47989, unique_id, uuid_str);
+    construct_url(url, sizeof(url), i == 0, server->serverInfo.address, "serverinfo", NULL);
 
     PHTTP_DATA data = http_create_data();
     if (data == NULL)
@@ -305,7 +309,7 @@ static int load_server_status(PSERVER_DATA server)
   return ret;
 }
 
-static void bytes_to_hex(unsigned char *in, char *out, size_t len)
+static void bytes_to_hex(const unsigned char *in, char *out, size_t len)
 {
   for (int i = 0; i < len; i++)
   {
@@ -314,20 +318,29 @@ static void bytes_to_hex(unsigned char *in, char *out, size_t len)
   out[len * 2] = 0;
 }
 
-static bool sign_it(const unsigned char *msg, size_t mlen, unsigned char *sig, size_t *slen, mbedtls_pk_context *pkey, mbedtls_ctr_drbg_context *rng)
+static void hex_to_bytes(const char *in, unsigned char *out, size_t *len)
+{
+  size_t inl = strlen(in);
+  for (int count = 0; count < inl; count += 2)
+  {
+    sscanf(&in[count], "%2hhx", &out[count / 2]);
+  }
+  if (len)
+  {
+    *len = inl / 2;
+  }
+}
+
+static bool generateSignature(const unsigned char *msg, size_t mlen, unsigned char *sig, size_t *slen, mbedtls_pk_context *pkey, mbedtls_ctr_drbg_context *rng)
 {
   int result = 0;
-
-  *slen = 0;
-
   unsigned char hash[32];
   if ((result = mbedtls_sha256_ret(msg, mlen, hash, 0)) != 0)
   {
     goto cleanup;
   }
 
-  mbedtls_rsa_context *rsa = mbedtls_pk_rsa(*pkey);
-  if ((result = mbedtls_rsa_pkcs1_sign(rsa, mbedtls_ctr_drbg_random, rng, MBEDTLS_RSA_PRIVATE, MBEDTLS_MD_SHA256, 32, hash, sig)) != 0)
+  if ((result = mbedtls_pk_sign(pkey, MBEDTLS_MD_SHA256, hash, 32, sig, slen, mbedtls_ctr_drbg_random, rng)) != 0)
   {
     goto cleanup;
   }
@@ -336,27 +349,17 @@ cleanup:
   return result == 0;
 }
 
-static bool verifySignature(const unsigned char *data, int dataLength, unsigned char *signature, int signatureLength, const char *cert)
+static bool verifySignature(const unsigned char *msg, size_t mlen, const unsigned char *sig, size_t slen, mbedtls_x509_crt *cert)
 {
   int result = 0;
-  mbedtls_x509_crt x509;
-  mbedtls_x509_crt_init(&x509);
-
-  if ((result = mbedtls_x509_crt_parse(&x509, cert, strlen(cert) + 1)) != 0)
-  {
-    goto cleanup;
-  }
-
   unsigned char hash[32];
-  if ((result = mbedtls_sha256_ret(data, dataLength, hash, 0)) != 0)
+  if ((result = mbedtls_sha256_ret(msg, mlen, hash, 0)) != 0)
   {
-    goto cleanup;
+    return false;
   }
 
-  result = mbedtls_pk_verify(&x509.pk, MBEDTLS_MD_SHA256, hash, 32, signature, signatureLength);
+  result = mbedtls_pk_verify(&cert->pk, MBEDTLS_MD_SHA256, hash, 32, sig, slen);
 
-cleanup:
-  mbedtls_x509_crt_free(&x509);
   return result == 0;
 }
 
@@ -372,15 +375,25 @@ int gs_unpair(PSERVER_DATA server)
 
   uuid_generate_random(uuid);
   uuid_unparse(uuid, uuid_str);
-  snprintf(url, sizeof(url), "http://%s:47989/unpair?uniqueid=%s&uuid=%s", server->serverInfo.address, unique_id, uuid_str);
+  construct_url(url, sizeof(url), false, server->serverInfo.address, "unpair", NULL);
   ret = http_request(url, data);
 
   http_free_data(data);
   return ret;
 }
 
+struct challenge_response_t
+{
+  unsigned char challenge[16];
+  unsigned char signature[256];
+  unsigned char secret[16];
+};
+
 int gs_pair(PSERVER_DATA server, char *pin)
 {
+  int svrversion = server->serverMajorVersion;
+  mbedtls_md_type_t hash_algo = svrversion >= 7 ? MBEDTLS_MD_SHA256 : MBEDTLS_MD_SHA1;
+  size_t hash_length;
   int ret = GS_OK;
   char *result = NULL;
   char url[4096];
@@ -396,6 +409,9 @@ int gs_pair(PSERVER_DATA server, char *pin)
   mbedtls_aes_context aes;
   mbedtls_aes_init(&aes);
 
+  mbedtls_x509_crt server_cert;
+  mbedtls_x509_crt_init(&server_cert);
+
   if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0)) != 0)
   {
     goto cleanup;
@@ -403,24 +419,33 @@ int gs_pair(PSERVER_DATA server, char *pin)
 
   if (server->paired)
   {
+    ret = GS_WRONG_STATE;
     gs_error = "Already paired";
-    return GS_WRONG_STATE;
+    goto cleanup;
   }
 
   if (server->currentGame != 0)
   {
+    ret = GS_WRONG_STATE;
     gs_error = "The computer is currently in a game. You must close the game before pairing";
-    return GS_WRONG_STATE;
+    goto cleanup;
   }
 
-  unsigned char salt_data[16];
+  unsigned char salt[16];
+  mbedtls_ctr_drbg_random(&ctr_drbg, salt, 16);
   char salt_hex[33];
-  mbedtls_ctr_drbg_random(&ctr_drbg, salt_data, 16);
-  bytes_to_hex(salt_data, salt_hex, 16);
+  bytes_to_hex(salt, salt_hex, 16);
 
-  uuid_generate_random(uuid);
-  uuid_unparse(uuid, uuid_str);
-  snprintf(url, sizeof(url), "http://%s:47989/pair?uniqueid=%s&uuid=%s&devicename=roth&updateState=1&phrase=getservercert&salt=%s&clientcert=%s", server->serverInfo.address, unique_id, uuid_str, salt_hex, cert_hex);
+  unsigned char salted_pin[20];
+  memcpy(salted_pin, salt, 16);
+  memcpy(&salted_pin[16], pin, 4);
+
+  unsigned char aes_key[32];
+
+  hash_data(hash_algo, salted_pin, 20, aes_key, &hash_length);
+
+  construct_url(url, sizeof(url), false, server->serverInfo.address, "pair",
+                "devicename=roth&updateState=1&phrase=getservercert&salt=%s&clientcert=%s", salt_hex, cert_hex);
   PHTTP_DATA data = http_create_data();
   if (data == NULL)
     return GS_OUT_OF_MEMORY;
@@ -455,37 +480,40 @@ int gs_pair(PSERVER_DATA server, char *pin)
     goto cleanup;
   }
 
-  char plaincert[8192];
-  for (int count = 0; count < strlen(result); count += 2)
+  char server_cert_str[8192];
+  size_t plaincert_len = 0;
+  hex_to_bytes(result, server_cert_str, &plaincert_len);
+  server_cert_str[plaincert_len] = 0;
+
+  if ((ret = mbedtls_x509_crt_parse(&server_cert, server_cert_str, plaincert_len + 1)) != 0)
   {
-    sscanf(&result[count], "%2hhx", &plaincert[count / 2]);
+    char errstr[4096];
+    mbedtls_strerror(ret, errstr, 4096);
+    printf("%s\n", errstr);
+    gs_error = "Failed to parse server cert";
+    ret = GS_FAILED;
+    goto cleanup;
   }
-  plaincert[strlen(result) / 2] = '\0';
 
-  unsigned char salt_pin[20];
-  unsigned char aes_key_hash[32];
-  memcpy(salt_pin, salt_data, 16);
-  memcpy(salt_pin + 16, pin, 4);
+  mbedtls_aes_setkey_enc(&aes, aes_key, 128);
+  mbedtls_aes_setkey_dec(&aes, aes_key, 128);
 
-  int hash_length = server->serverMajorVersion >= 7 ? 32 : 20;
-  if (server->serverMajorVersion >= 7)
-    mbedtls_sha256_ret(salt_pin, 20, aes_key_hash, 0);
-  else
-    mbedtls_sha1_ret(salt_pin, 20, aes_key_hash);
+  unsigned char random_challenge[16];
+  mbedtls_ctr_drbg_random(&ctr_drbg, random_challenge, 16);
 
-  mbedtls_aes_setkey_enc(&aes, aes_key_hash, 128);
-  mbedtls_aes_setkey_dec(&aes, aes_key_hash, 128);
+  unsigned char encrypted_challenge[16];
+  if (!crypt_data(&aes, MBEDTLS_AES_ENCRYPT, random_challenge, encrypted_challenge, 16))
+  {
+    gs_error = "Failed to encrypt random_challenge";
+    ret = GS_FAILED;
+    goto cleanup;
+  }
 
-  unsigned char challenge_data[16];
-  unsigned char challenge_enc[16];
-  char challenge_hex[33];
-  mbedtls_ctr_drbg_random(&ctr_drbg, challenge_data, 16);
-  mbedtls_aes_encrypt(&aes, challenge_data, challenge_enc);
-  bytes_to_hex(challenge_enc, challenge_hex, 16);
+  char encrypted_challenge_hex[33];
+  bytes_to_hex(encrypted_challenge, encrypted_challenge_hex, 16);
 
-  uuid_generate_random(uuid);
-  uuid_unparse(uuid, uuid_str);
-  snprintf(url, sizeof(url), "http://%s:47989/pair?uniqueid=%s&uuid=%s&devicename=roth&updateState=1&clientchallenge=%s", server->serverInfo.address, unique_id, uuid_str, challenge_hex);
+  construct_url(url, sizeof(url), false, server->serverInfo.address, "pair",
+                "devicename=roth&updateState=1&clientchallenge=%s", encrypted_challenge_hex);
   if ((ret = http_request(url, data)) != GS_OK)
     goto cleanup;
 
@@ -513,40 +541,31 @@ int gs_pair(PSERVER_DATA server, char *pin)
 
   unsigned char challenge_response_data_enc[48];
   unsigned char challenge_response_data[48];
-  for (int count = 0; count < strlen(result); count += 2)
-  {
-    sscanf(&result[count], "%2hhx", &challenge_response_data_enc[count / 2]);
-  }
+  hex_to_bytes(result, challenge_response_data_enc, NULL);
 
-  for (int i = 0; i < 48; i += 16)
-  {
-    mbedtls_aes_decrypt(&aes, &challenge_response_data_enc[i], &challenge_response_data[i]);
-  }
+  crypt_data(&aes, MBEDTLS_AES_DECRYPT, challenge_response_data_enc, challenge_response_data, 48);
 
   unsigned char client_secret_data[16];
   mbedtls_ctr_drbg_random(&ctr_drbg, client_secret_data, 16);
 
-  unsigned char challenge_response[16 + 256 + 16];
-  unsigned char challenge_response_hash[32];
-  unsigned char challenge_response_hash_enc[32];
-  char challenge_response_hex[65];
-  memcpy(challenge_response, challenge_response_data + hash_length, 16);
-  memcpy(challenge_response + 16, cert.sig.p, 256);
-  memcpy(challenge_response + 16 + 256, client_secret_data, 16);
+  struct challenge_response_t challenge_response;
+  unsigned char challenge_response_hash[32], challenge_response_hash_enc[32];
+  memset(challenge_response_hash, 0, sizeof(challenge_response_hash));
+  memcpy(challenge_response.challenge, &challenge_response_data[hash_length], sizeof(challenge_response.challenge));
+  memcpy(challenge_response.signature, cert.sig.p, sizeof(challenge_response.signature));
+  memcpy(challenge_response.secret, client_secret_data, sizeof(challenge_response.secret));
   if (server->serverMajorVersion >= 7)
-    mbedtls_sha256_ret(challenge_response, 16 + 256 + 16, challenge_response_hash, 0);
+    mbedtls_sha256_ret((unsigned char *)&challenge_response, sizeof(struct challenge_response_t), challenge_response_hash, 0);
   else
-    mbedtls_sha1_ret(challenge_response, 16 + 256 + 16, challenge_response_hash);
+    mbedtls_sha1_ret((unsigned char *)&challenge_response, sizeof(struct challenge_response_t), challenge_response_hash);
 
-  for (int i = 0; i < 32; i += 16)
-  {
-    mbedtls_aes_encrypt(&aes, &challenge_response_hash[i], &challenge_response_hash_enc[i]);
-  }
+  crypt_data(&aes, MBEDTLS_AES_ENCRYPT, challenge_response_hash, challenge_response_hash_enc, 32);
+
+  char challenge_response_hex[65];
   bytes_to_hex(challenge_response_hash_enc, challenge_response_hex, 32);
 
-  uuid_generate_random(uuid);
-  uuid_unparse(uuid, uuid_str);
-  snprintf(url, sizeof(url), "http://%s:47989/pair?uniqueid=%s&uuid=%s&devicename=roth&updateState=1&serverchallengeresp=%s", server->serverInfo.address, unique_id, uuid_str, challenge_response_hex);
+  construct_url(url, sizeof(url), false, server->serverInfo.address, "pair",
+                "updateState=1&serverchallengeresp=%s", challenge_response_hex);
   if ((ret = http_request(url, data)) != GS_OK)
     goto cleanup;
 
@@ -572,32 +591,47 @@ int gs_pair(PSERVER_DATA server, char *pin)
     goto cleanup;
   }
 
-  unsigned char pairing_secret[16 + 256];
-  for (int count = 0; count < strlen(result); count += 2)
+  struct
   {
-    sscanf(&result[count], "%2hhx", &pairing_secret[count / 2]);
-  }
+    unsigned char secret[16];
+    unsigned char signature[256];
+  } pairing_secret;
+  hex_to_bytes(result, (unsigned char *)&pairing_secret, NULL);
 
-  if (!verifySignature(pairing_secret, 16, pairing_secret + 16, 256, plaincert))
+  if (!verifySignature(pairing_secret.secret, sizeof(pairing_secret.secret),
+                       pairing_secret.signature, sizeof(pairing_secret.signature), &server_cert))
   {
     gs_error = "MITM attack detected";
     ret = GS_FAILED;
     goto cleanup;
   }
 
-  unsigned char signature[256];
+  struct challenge_response_t expected_response_data;
+  memcpy(expected_response_data.challenge, random_challenge, 16);
+  memcpy(expected_response_data.signature, server_cert.sig.p, 256);
+  memcpy(expected_response_data.secret, pairing_secret.secret, 16);
+
+  unsigned char expected_hash[32];
+  hash_data(hash_algo, (unsigned char *)&expected_response_data, sizeof(expected_response_data),
+            expected_hash, &hash_length);
+  if (memcmp(expected_hash, challenge_response_data, hash_length) != 0)
+  {
+    gs_error = "Incorrect PIN";
+    ret = GS_FAILED;
+    goto cleanup;
+  }
+
+  unsigned char client_pairing_secret[16 + 256];
+  memcpy(client_pairing_secret, client_secret_data, 16);
   size_t s_len;
-  if (!sign_it(client_secret_data, 16, signature, &s_len, &privateKey, &ctr_drbg))
+  if (!generateSignature(client_pairing_secret, 16, &client_pairing_secret[16], &s_len, &privateKey, &ctr_drbg))
   {
     gs_error = "Failed to sign data";
     ret = GS_FAILED;
     goto cleanup;
   }
 
-  unsigned char client_pairing_secret[16 + 256];
   char client_pairing_secret_hex[(16 + 256) * 2 + 1];
-  memcpy(client_pairing_secret, client_secret_data, 16);
-  memcpy(&client_pairing_secret[16], signature, 256);
   bytes_to_hex(client_pairing_secret, client_pairing_secret_hex, 16 + 256);
 
   uuid_generate_random(uuid);
@@ -652,7 +686,9 @@ cleanup:
   http_free_data(data);
 
   mbedtls_aes_free(&aes);
+  mbedtls_entropy_free(&entropy);
   mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_x509_crt_free(&server_cert);
 
   return ret;
 }
@@ -861,4 +897,57 @@ int gs_init(PSERVER_DATA server, char *address, const char *keyDirectory, int lo
   server->serverInfo.address = address;
   server->unsupported = unsupported;
   return load_server_status(server);
+}
+
+static bool construct_url(char *url, size_t ulen, bool secure, const char *address, const char *action, const char *fmt, ...)
+{
+  uuid_t uuid;
+  char uuid_str[37];
+
+  uuid_generate_random(uuid);
+  uuid_unparse(uuid, uuid_str);
+
+  if (fmt)
+  {
+    char params[4096];
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(params, 4096, fmt, ap);
+    va_end(ap);
+    snprintf(url, ulen, "%s://%s:%d/%s?uniqueid=%s&uuid=%s&%s", secure ? "https" : "http", address, secure ? 47984 : 47989,
+             action, unique_id, uuid_str, params);
+  }
+  else
+  {
+    snprintf(url, ulen, "%s://%s:%d/%s?uniqueid=%s&uuid=%s", secure ? "https" : "http", address, secure ? 47984 : 47989,
+             action, unique_id, uuid_str);
+  }
+  return true;
+}
+
+int hash_data(mbedtls_md_type_t type, const unsigned char *input, size_t ilen, unsigned char *output, size_t *olen)
+{
+  switch (type)
+  {
+  case MBEDTLS_MD_SHA1:
+    *olen = 20;
+    return mbedtls_sha1_ret(input, ilen, output);
+  case MBEDTLS_MD_SHA256:
+    *olen = 32;
+    return mbedtls_sha256_ret(input, ilen, output, 0);
+  default:
+    return -1;
+  }
+}
+
+bool crypt_data(mbedtls_aes_context *ctx, int mode, const unsigned char *input, unsigned char *output, size_t len)
+{
+  for (int i = 0; i < len; i += 16)
+  {
+    if (mbedtls_aes_crypt_ecb(ctx, mode, input, output) != 0)
+    {
+      return false;
+    }
+  }
+  return true;
 }
