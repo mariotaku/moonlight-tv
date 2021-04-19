57d8ddd14677a0d83778fd425b58fde95ff89942
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-20 00:01:42 +0900

    Easy build script for webOS

---
+++ scripts/webos/easy_build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+TOPDIR=$(git rev-parse --show-toplevel)
+
+if [ $TOPDIR != $PWD ]; then
+    echo "Please invoke this script in project root directory"
+    return 1
+fi
+
+echo "Update submodules"
+git submodule update --init --recursive
+
+echo "Project configuration"
+cd build
+cmake .. -DTARGET_WEBOS=ON -DCMAKE_TOOLCHAIN_FILE=/opt/webos-sdk-x86_64/1.0.g/sysroots/x86_64-webossdk-linux/usr/share/cmake/OEToolchainConfig.cmake
+
+echo "Start build"
+make webos-package-moonlight
