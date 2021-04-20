e407fc94b6702e3c5cff689c5ef91911258edfeb
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-20 16:53:30 +0900

    Logging for NDL webOS 5

--- decoder/ndl/ndl_webos5.c
+++ decoder/ndl/ndl_webos5.c
@@ -1,9 +1,12 @@
 #include "ndl_common.h"
 
+#include <stdio.h>
+
 bool media_loaded = false;
 
 NDL_DIRECTMEDIA_DATA_INFO media_info;
 
 void media_load_callback(int type, long long numValue, const char *strValue)
 {
+    printf("[NDL] MediaLoadCallback type=%d, numValue=%x, strValue=%p\n");
 }
