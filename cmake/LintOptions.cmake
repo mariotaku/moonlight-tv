ef5619dea29ce2cb7b03a5c7db803de3c774a263
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-22 20:30:46 +0900

    Added compile lint for modules

--- cmake/LintOptions.cmake
+++ cmake/LintOptions.cmake
@@ -4,6 +4,6 @@ function(target_compile_lint TARGET)
   if (CMAKE_C_COMPILER_ID STREQUAL "Clang")
     # using Clang
   elseif (CMAKE_C_COMPILER_ID STREQUAL "GNU")
-    target_compile_options(${TARGET} PRIVATE -Werror=return-type)
+    target_compile_options(${TARGET} PRIVATE -Werror=return-type -Werror=implicit-function-declaration)
   endif()
 endfunction()
