afe40c29b1659c30d2bb1f6c29c76393c8010ea7
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-25 18:57:41 +0900

    Fixed video pts unit

--- decoder/smp/src/AVStreamPlayer.cpp
+++ decoder/smp/src/AVStreamPlayer.cpp
@@ -105,7 +105,8 @@ int AVStreamPlayer::submitVideo(PDECODE_UNIT decodeUnit)
         return DR_NEED_IDR;
     }
     unsigned long long ms = decodeUnit->presentationTimeMs;
-    video_pts_ = ms * 1000ULL;
+    // In nanoseconds.
+    video_pts_ = ms * 1000000ULL;
 
     int length = 0;
     for (PLENTRY entry = decodeUnit->bufferList; entry != NULL; entry = entry->next)
