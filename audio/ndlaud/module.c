8bff398854a18f010b0779cdfd100eeecae50eb0
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-05-06 22:18:46 +0900

    Removed NDL audio check to prevent crash

--- audio/ndlaud/module.c
+++ audio/ndlaud/module.c
@@ -30,36 +30,10 @@ bool audio_init_ndlaud(int argc, char *argv[], PHOST_CONTEXT hctx)
     return ndl_initialized;
 }
 
-const static unsigned int channelChecks[] = {
-    // AUDIO_CONFIGURATION_71_SURROUND,
-    // AUDIO_CONFIGURATION_51_SURROUND,
-    AUDIO_CONFIGURATION_STEREO,
-};
-const static int channelChecksCount = sizeof(channelChecks) / sizeof(unsigned int);
-
 bool audio_check_ndlaud(PAUDIO_INFO ainfo)
 {
-    for (int i = 0; i < channelChecksCount; i++)
-    {
-        NDL_DIRECTAUDIO_DATA_INFO info = {
-            .numChannel = CHANNEL_COUNT_FROM_AUDIO_CONFIGURATION(channelChecks[i]),
-            .bitPerSample = 16,
-            .nodelay = 1,
-            .upperThreshold = 48,
-            .lowerThreshold = 16,
-            .channel = NDL_DIRECTAUDIO_CH_MAIN,
-            .srcType = NDL_DIRECTAUDIO_SRC_TYPE_PCM,
-            .samplingFreq = NDL_DIRECTAUDIO_SAMPLING_FREQ_OF(48000),
-        };
-        if (NDL_DirectAudioOpen(&info) == 0)
-        {
-            NDL_DirectAudioClose();
-            ainfo->valid = true;
-            ainfo->configuration = channelChecks[i];
-            return true;
-        }
-    }
-    return false;
+    ainfo->valid = true;
+    return true;
 }
 
 void audio_finalize_ndlaud()
