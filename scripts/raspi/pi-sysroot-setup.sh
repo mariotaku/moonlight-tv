b7307c6a16a8c1b6ea730a51e8a55ae2449e2e8d
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-08-01 05:35:59 +0900

    Updating sysroot setup script

--- scripts/raspi/pi-sysroot-setup.sh
+++ scripts/raspi/pi-sysroot-setup.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
 
-SYSROOT=/opt/pi-sysroot
-
-# Use qemu to support cross-architecture debootstrap
-sudo apt-get install qemu-user-static
-
-# Create a fake rootfs
-sudo qemu-debootstrap --arch=armhf --no-check-gpg buster ${SYSROOT} http://raspbian.raspberrypi.org/raspbian/
-
-sudo update-binfmts --enable qemu-arm
-
-# Run installation command in chroot environment
-cat << CHROOT_SCRIPT | sudo chroot $SYSROOT
-
 # Use Google DNS for a consistant name solution 
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
@@ -29,10 +16,8 @@ apt-get -y update
 apt-get install symlinks
 
 # Install libraries and headers
-apt-get install -y libsdl2-dev libsdl2-image-dev libopus-dev uuid-dev    \
+apt-get install -y libsdl2-dev libsdl2-image-dev libopus-dev uuid-dev         \
      libcurl4-openssl-dev libavcodec-dev libavutil-dev libexpat1-dev          \
      libmbedtls-dev libfontconfig1-dev libraspberrypi-dev libconfig-dev
 
 symlinks -cr /usr/lib
-
-CHROOT_SCRIPT
