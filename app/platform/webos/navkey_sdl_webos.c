ce4a0579fc4e0fe0bcbbfd31e5aab374d1e7121b
Mariotaku <mariotaku.lee@gmail.com>
2021-08-31 01:27:40 +0900

    fixing build on webos

--- app/platform/webos/navkey_sdl_webos.c
+++ app/platform/webos/navkey_sdl_webos.c
@@ -1,14 +1,14 @@
 #include "platform/sdl/navkey_sdl.h"
 #include <SDL_webOS.h>
 
-NAVKEY navkey_from_sdl_webos(SDL_Event ev)
+NAVKEY navkey_from_sdl_webos(SDL_Event *ev)
 {
-    switch (ev.type)
+    switch (ev->type)
     {
     case SDL_KEYDOWN:
     case SDL_KEYUP:
     {
-        switch (ev.key.keysym.scancode)
+        switch (ev->key.keysym.scancode)
         {
         case SDL_WEBOS_SCANCODE_RED:
             return NAVKEY_NEGATIVE;
