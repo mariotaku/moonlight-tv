199474b1a366d53fd12d30549d779f8b13bfcbb4
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-09-20 22:59:09 +0900

    improved soft keyboard handling

--- app/lvgl/lv_sdl_drv_key_input.c
+++ app/lvgl/lv_sdl_drv_key_input.c
@@ -27,6 +27,38 @@ static void webos_key_input_mode(const SDL_KeyboardEvent *event);
 
 #endif
 
+static void sdl_input_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
+
+lv_indev_t *lv_sdl_init_key_input() {
+    lv_group_t *group = lv_group_get_default();
+
+    lv_indev_drv_t *indev_drv = malloc(sizeof(lv_indev_drv_t));
+    lv_indev_drv_init(indev_drv);
+    indev_key_state_t *state = malloc(sizeof(indev_key_state_t));
+    lv_memset_00(state, sizeof(indev_key_state_t));
+    indev_drv->user_data = state;
+    indev_drv->type = LV_INDEV_TYPE_KEYPAD;
+    indev_drv->read_cb = sdl_input_read;
+
+    state->state = LV_INDEV_STATE_RELEASED;
+
+    lv_indev_t *indev = lv_indev_drv_register(indev_drv);
+    if (group) {
+        lv_indev_set_group(indev, group);
+    }
+    return indev;
+}
+
+void lv_sdl_key_input_release_key(lv_indev_t *indev) {
+    indev_key_state_t *state = indev->driver->user_data;
+    state->state = LV_INDEV_STATE_RELEASED;
+}
+
+void lv_sdl_deinit_key_input(lv_indev_t *indev) {
+    free(indev->driver->user_data);
+    free(indev->driver);
+}
+
 static void sdl_input_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
     indev_key_state_t *state = drv->user_data;
     SDL_Event e;
@@ -80,31 +112,6 @@ static void sdl_input_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
     }
     data->key = state->key;
     data->state = state->state;
-}
-
-lv_indev_t *lv_sdl_init_key_input() {
-    lv_group_t *group = lv_group_get_default();
-
-    lv_indev_drv_t *indev_drv = malloc(sizeof(lv_indev_drv_t));
-    lv_indev_drv_init(indev_drv);
-    indev_key_state_t *state = malloc(sizeof(indev_key_state_t));
-    lv_memset_00(state, sizeof(indev_key_state_t));
-    indev_drv->user_data = state;
-    indev_drv->type = LV_INDEV_TYPE_KEYPAD;
-    indev_drv->read_cb = sdl_input_read;
-
-    state->state = LV_INDEV_STATE_RELEASED;
-
-    lv_indev_t *indev = lv_indev_drv_register(indev_drv);
-    if (group) {
-        lv_indev_set_group(indev, group);
-    }
-    return indev;
-}
-
-void lv_sdl_deinit_key_input(lv_indev_t *indev) {
-    free(indev->driver->user_data);
-    free(indev->driver);
 }
 
 static bool read_event(const SDL_Event *event, indev_key_state_t *state) {
