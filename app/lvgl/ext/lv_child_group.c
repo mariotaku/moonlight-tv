f2a5b9ed83a1a34cb10eae0fa9b33bb27c9d8c24
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-10-01 01:07:00 +0900

    fixed group addition

--- app/lvgl/ext/lv_child_group.c
+++ app/lvgl/ext/lv_child_group.c
@@ -2,7 +2,7 @@
 
 void cb_child_group_add(lv_event_t *event) {
     lv_obj_t *child = lv_event_get_param(event);
-    if (!child || !lv_obj_get_group(child)) return;
+    if (!child || !lv_obj_is_group_def(child)) return;
     lv_group_t *group = lv_event_get_user_data(event);
     lv_group_add_obj(group, child);
 }
