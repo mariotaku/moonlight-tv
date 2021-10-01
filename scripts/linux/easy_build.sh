e46a1460a551c02878a28b0c5ff9fb343aa040e2
Curtis S. Alexander <exalented@gmail.com>
2021-09-30 23:22:46 -0700

    Modify easy_build script to support Arch.

--- scripts/linux/easy_build.sh
+++ scripts/linux/easy_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TOPDIR=$(git rev-parse --show-toplevel)
+#Get kernel release name
+os=$(uname -r)
 
 if [ $TOPDIR != $PWD ]; then
     echo "Please invoke this script in project root directory"
@@ -14,10 +16,19 @@ echo "Update submodules"
 git submodule update --init --recursive
 
 echo "Install dependencies. You may be prompted to enter password"
-sudo apt-get install -y build-essential cmake
-sudo apt-get install -y libsdl2-dev libsdl2-image-dev libopus-dev uuid-dev    \
-     libcurl4-openssl-dev libavcodec-dev libavutil-dev libexpat1-dev          \
-     libmbedtls-dev libfontconfig1-dev libconfig-dev
+case $os in
+  *arch*|*Arch*)
+	echo Detected system as Arch base running kernel $os;
+	sudo pacman -S openssl cmake ffmpeg sdl2_image sdl2 opus curl mbedtls fontconfig expat
+    ;;
+  *)
+	echo Detected system as Debian base running kernel $os;
+	sudo apt-get install -y build-essential cmake
+	sudo apt-get install -y libsdl2-dev libsdl2-image-dev libopus-dev uuid-dev    \
+	     libcurl4-openssl-dev libavcodec-dev libavutil-dev libexpat1-dev          \
+	     libmbedtls-dev libfontconfig1-dev libconfig-dev
+    ;;
+esac
 
 echo "Project configuration"
 if [ ! -d build ]; then
