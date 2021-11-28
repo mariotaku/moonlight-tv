e29143c39c7644f242c041f38e5a495869100892
Mariotaku <mariotaku.lee@gmail.com>
2021-11-29 00:51:05 +0900

    improved keyboard for help dialog

--- app/ui/help/help.dialog.c
+++ app/ui/help/help.dialog.c
@@ -3,6 +3,8 @@
 
 #include "util/i18n.h"
 
+static void menu_key_cb(lv_event_t *e);
+
 static void help_dialog_resize_cb(lv_event_t *e);
 
 static void help_open_url_cb(lv_event_t *e);
@@ -16,10 +18,16 @@ lv_obj_t *help_dialog_create() {
     lv_obj_add_event_cb(msgbox, help_dialog_resize_cb, LV_EVENT_SIZE_CHANGED, NULL);
     lv_obj_set_size(msgbox, LV_PCT(80), LV_PCT(90));
 
+    lv_obj_t *close = lv_msgbox_get_close_btn(msgbox);
+    lv_group_remove_obj(close);
+
     lv_obj_t *content = lv_msgbox_get_content(msgbox);
     lv_obj_set_flex_flow(content, LV_FLEX_FLOW_COLUMN);
+    lv_obj_add_flag(content, LV_OBJ_FLAG_EVENT_BUBBLE);
     lv_obj_t *list = lv_list_create(content);
+    lv_obj_add_event_cb(list, menu_key_cb, LV_EVENT_KEY, msgbox);
     lv_obj_set_size(list, LV_PCT(100), LV_PCT(100));
+    lv_obj_add_flag(list, LV_OBJ_FLAG_EVENT_BUBBLE);
     lv_obj_set_style_border_side(list, LV_BORDER_SIDE_NONE, 0);
     lv_obj_set_style_radius(list, 0, 0);
     lv_obj_set_style_pad_all(list, 0, 0);
@@ -40,6 +48,14 @@ lv_obj_t *help_dialog_create() {
     return msgbox;
 }
 
+static void menu_key_cb(lv_event_t *e) {
+    lv_obj_t *target = lv_event_get_target(e);
+    if (target->parent != lv_event_get_current_target(e)) return;
+    if (lv_event_get_key(e) == LV_KEY_ESC) {
+        lv_msgbox_close_async(lv_event_get_user_data(e));
+    }
+}
+
 static void help_dialog_resize_cb(lv_event_t *e) {
     lv_obj_t *msgbox = lv_event_get_current_target(e);
     lv_obj_t *title = lv_msgbox_get_title(msgbox);
@@ -56,5 +72,6 @@ static void help_open_url_cb(lv_event_t *e) {
 
 static void add_url_button(lv_obj_t *list, const char *title, const char *url) {
     lv_obj_t *item = lv_list_add_btn(list, NULL, locstr(title));
+    lv_obj_add_flag(item, LV_OBJ_FLAG_EVENT_BUBBLE);
     lv_obj_add_event_cb(item, help_open_url_cb, LV_EVENT_CLICKED, (void *) url);
 }
