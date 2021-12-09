f8daba3e3d0ce2765b14df575e3286244a11e2d5
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-12-09 21:59:42 +0900

    updated gen_gamecontrollerdb.sh

--- scripts/webos/gen_gamecontrollerdb.sh
+++ scripts/webos/gen_gamecontrollerdb.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ ! -d assets ]; then
-    mkdir assets
+OUTDIR=deploy/webos/assets
+
+if [ ! -d $OUTDIR ]; then
+    mkdir -p $OUTDIR
 fi
 
-echo "# Generated from https://github.com/gabomdq/SDL_GameControllerDB" > assets/gamecontrollerdb.txt
-git -C third_party/SDL_GameControllerDB log -1 --pretty=format:"# %h %ad - %s%n" >> assets/gamecontrollerdb.txt
-echo >> assets/gamecontrollerdb.txt
-grep ',platform:Linux,$' third_party/SDL_GameControllerDB/gamecontrollerdb.txt | sed 's/,platform:Linux,$/,platform:webOS,/' >> assets/gamecontrollerdb.txt
+echo "# Generated from https://github.com/gabomdq/SDL_GameControllerDB" > $OUTDIR/gamecontrollerdb.txt
+echo "# Generated at $(date)" >> $OUTDIR/gamecontrollerdb.txt
+echo >> $OUTDIR/gamecontrollerdb.txt
+curl -L 'https://github.com/gabomdq/SDL_GameControllerDB/raw/master/gamecontrollerdb.txt' \
+ | grep ',platform:Linux,$'\
+ | sed 's/,platform:Linux,$/,platform:webOS,/' >> $OUTDIR/gamecontrollerdb.txt
