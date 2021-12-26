2e2b5e4ee65782d101921271324e47bb344df6a8
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-12-26 13:55:43 +0900

    updated webos build to use cpack

--- cmake/PackageWebOS.cmake
+++ cmake/PackageWebOS.cmake
@@ -2,6 +2,7 @@
 install(DIRECTORY deploy/webos/ DESTINATION ${CMAKE_INSTALL_WEBOS_PKGDIR} PATTERN ".*" EXCLUDE PATTERN "*.in" EXCLUDE)
 # Copy manifest
 configure_file(deploy/webos/appinfo.json.in ${CMAKE_INSTALL_WEBOS_PKGDIR}/appinfo.json @ONLY)
+configure_file(cmake/CPackWebOS.cmake.in ${CMAKE_BINARY_DIR}/CPackWebOS.cmake @ONLY)
 
 add_custom_target(webos-generate-gamecontrollerdb
         COMMAND ${CMAKE_SOURCE_DIR}/scripts/webos/gen_gamecontrollerdb.sh
@@ -9,6 +10,9 @@ add_custom_target(webos-generate-gamecontrollerdb
         )
 
 set(WEBOS_PACKAGE_FILENAME ${WEBOS_APPINFO_ID}_${PROJECT_VERSION}_$ENV{ARCH}.ipk)
+
+set(CPACK_GENERATOR "External")
+set(CPACK_EXTERNAL_PACKAGE_SCRIPT "${CMAKE_BINARY_DIR}/CPackWebOS.cmake")
 
 add_custom_target(webos-package-moonlight
         COMMAND ${CMAKE_COMMAND} --build . --target install
@@ -23,4 +27,4 @@ set_target_properties(moonlight PROPERTIES
         WEBOS_APPINFO_ID ${WEBOS_APPINFO_ID}
         )
 
-# add_dependencies(webos-package-moonlight webos-generate-gamecontrollerdb)
+include(CPack)
--- scripts/webos/easy_build.sh
+++ scripts/webos/easy_build.sh
@@ -1,35 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 
 TOPDIR=$(git rev-parse --show-toplevel)
 
-if [ $TOPDIR != $PWD ]; then
-    echo "Please invoke this script in project root directory"
-    exit 1
+if [ "$TOPDIR" != "$PWD" ]; then
+  echo "Please invoke this script in project root directory"
+  exit 1
 fi
 
 CMAKE_BIN=$(which cmake)
-
-if [ -z $CMAKE_BIN ]; then
-    echo "Please install CMake."
-    exit 1
+CPACK_BIN=$(which cpack)
+if [ -z "${CMAKE_BINARY_DIR}" ]; then
+  CMAKE_BINARY_DIR=build
 fi
 
-if [ -z $CI ]; then
-    echo "Update submodules"
-    git submodule update --init --recursive
+if [ ! -x "$CMAKE_BIN" ]; then
+  echo "Please install CMake."
+  exit 1
+fi
+
+if [ -z "$CI" ]; then
+  echo "Update submodules"
+  git submodule update --init --recursive
 fi
 
 echo "Setup environment"
 . /opt/webos-sdk-x86_64/1.0.g/environment-setup-armv7a-neon-webos-linux-gnueabi
+TOOLCHAIN_FILE=/opt/webos-sdk-x86_64/1.0.g/sysroots/x86_64-webossdk-linux/usr/share/cmake/OEToolchainConfig.cmake
 
 echo "Configure project"
-if [ ! -d build ]; then
-    mkdir build
+if [ ! -d "${CMAKE_BINARY_DIR}" ]; then
+  mkdir -p "${CMAKE_BINARY_DIR}"
 fi
 
-cd build
-$CMAKE_BIN .. -DCMAKE_TOOLCHAIN_FILE=/opt/webos-sdk-x86_64/1.0.g/sysroots/x86_64-webossdk-linux/usr/share/cmake/OEToolchainConfig.cmake -DTARGET_WEBOS=ON $@
+# shellcheck disable=SC2068
+$CMAKE_BIN -B"${CMAKE_BINARY_DIR}" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" -DTARGET_WEBOS=ON -DBUILD_TESTS=OFF $@ || exit 1
+
+$CMAKE_BIN --build "${CMAKE_BINARY_DIR}" --target install -- -j "$(nproc)" || exit 1
 
 echo "Build package"
-$CMAKE_BIN --install .
-$CMAKE_BIN --build . --target webos-package-moonlight
+cd "${CMAKE_BINARY_DIR}" || exit 1
+$CPACK_BIN
