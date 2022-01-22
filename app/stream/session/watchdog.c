7ba7d9d3546fec66075f3aee8dcf1ce828b6e5dd
Mariotaku <mariotaku.lee@gmail.com>
2022-01-22 19:49:02 +0900

    removed usage to sdl_atomic

--- app/stream/session/watchdog.c
+++ app/stream/session/watchdog.c
@@ -7,13 +7,13 @@
 #define CHECK_TIMEOUT 3000
 
 static SDL_TimerID timer = 0;
-static SDL_atomic_t counter;
+static int counter;
 
 static Uint32 watchdog_check();
 
 void streaming_watchdog_start() {
     if (timer) return;
-    SDL_AtomicSet(&counter, 0);
+    counter = 0;
     timer = SDL_AddTimer(CHECK_INTERVAL, watchdog_check, NULL);
 }
 
@@ -24,16 +24,17 @@ void streaming_watchdog_stop() {
 }
 
 void streaming_watchdog_reset() {
-    SDL_AtomicSet(&counter, 0);
+    counter = 0;
 }
 
 static Uint32 watchdog_check() {
-    int count = SDL_AtomicAdd(&counter, 1);
+    int count = counter;
     // Interrupt the stream if no frame received over timeout
     if (count >= (CHECK_TIMEOUT / CHECK_INTERVAL)) {
         streaming_interrupt(false, STREAMING_INTERRUPT_WATCHDOG);
         return 0;
     }
+    counter++;
     vdec_stat_refresh();
     return CHECK_INTERVAL;
 }
