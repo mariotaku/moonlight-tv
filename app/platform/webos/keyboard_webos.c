5cb1919df3f270b08a0a36b44e5da0242c142dc9
Mariotaku Lee <mariotaku.lee@gmail.com>
2022-05-02 23:13:30 +0900

    improved webos exit button

--- app/platform/webos/keyboard_webos.c
+++ app/platform/webos/keyboard_webos.c
@@ -1,3 +1,5 @@
+#include "app.h"
+
 #include "ui/root.h"
 
 #include "stream/input/absinput.h"
@@ -14,7 +16,11 @@ bool webos_intercept_remote_keys(SDL_KeyboardEvent *event, short *keyCode) {
     switch ((unsigned int) event->keysym.scancode) {
         case SDL_WEBOS_SCANCODE_EXIT: {
             if (event->state == SDL_PRESSED) {
-                bus_pushevent(USER_OPEN_OVERLAY, NULL, NULL);
+                if (streaming_running()) {
+                    bus_pushevent(USER_OPEN_OVERLAY, NULL, NULL);
+                } else {
+                    app_request_exit();
+                }
             }
             return true;
         }
