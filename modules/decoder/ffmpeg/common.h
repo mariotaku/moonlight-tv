69cb7850a38ff09d95fe104e367e7d22d9881753
Ningyuan Li <mariotaku.lee@gmail.com>
2023-02-25 20:21:07 +0900

    completely removed legacy module

--- modules/decoder/ffmpeg/common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "module/api.h"
-
-#define SDL_BUFFER_FRAMES 2
-
-extern int render_current_frame_ffmpeg, render_next_frame_ffmpeg;
-
-extern HOST_RENDER_CONTEXT *host_render_context_ffmpeg;
