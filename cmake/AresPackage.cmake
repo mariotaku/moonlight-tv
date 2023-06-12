a729b787a1279d93f887bd5a5c35f52b3a75996c
Ningyuan Li <mariotaku.lee@gmail.com>
2023-06-12 15:22:47 +0900

    updated build tools

--- cmake/AresPackage.cmake
+++ cmake/AresPackage.cmake
@@ -4,8 +4,7 @@ execute_process(COMMAND ares-package "${CPACK_TEMPORARY_DIRECTORY}" -o "${CPACK_
         -e "libmbedtls[.].*"
         )
 
-execute_process(COMMAND npm run webos-gen-manifest -- -a "${CPACK_TEMPORARY_DIRECTORY}/appinfo.json"
-        -p "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.ipk"
+execute_process(COMMAND webosbrew-gen-manifest -p "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.ipk"
         -o "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_NAME}.manifest.json"
         -i "https://github.com/mariotaku/moonlight-tv/raw/main/deploy/webos/icon.png"
         -l "https://github.com/mariotaku/moonlight-tv"
