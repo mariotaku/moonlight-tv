04393ae4a7d90825be1b5caceeee7bf2ec8316c7
Ningyuan Li <mariotaku.lee@gmail.com>
2023-07-21 11:07:00 +0900

    symlink-less package for webOS and steamlink changed steamlink packaging to zip

--- cmake/PackageSteamLink.cmake
+++ cmake/PackageSteamLink.cmake
@@ -1,12 +1,17 @@
+if (NOT DEFINED CMAKE_INSTALL_PREFIX)
+    set(CMAKE_INSTALL_PREFIX moonlight-tv)
+endif ()
+
 # Copy all files under deploy/webos/ to package root
-install(DIRECTORY deploy/steamlink/ DESTINATION . USE_SOURCE_PERMISSIONS PATTERN ".*" EXCLUDE)
+install(DIRECTORY deploy/steamlink/ DESTINATION ${CMAKE_INSTALL_PREFIX} USE_SOURCE_PERMISSIONS PATTERN ".*" EXCLUDE)
 
 set(CPACK_PACKAGE_NAME "moonlight-tv")
-set(CPACK_GENERATOR "TGZ")
+set(CPACK_GENERATOR "ZIP")
 set(CPACK_MONOLITHIC_INSTALL TRUE)
 set(CPACK_PACKAGE_DIRECTORY ${CMAKE_SOURCE_DIR}/dist)
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}_${PROJECT_VERSION}_steamlink")
 set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
+set(CPACK_PRE_BUILD_SCRIPTS "${CMAKE_SOURCE_DIR}/cmake/CleanupNameLink.cmake")
 # Will use all cores on CMake 3.20+
 set(CPACK_THREADS 0)
 
@@ -18,7 +23,7 @@ add_custom_target(steamlink-package-moonlight COMMAND cpack DEPENDS moonlight)
 
 if (NOT ENV{CI})
     add_custom_target(steamlink-install-moonlight
-            COMMAND ssh steamlink 'mkdir -p /home/apps/moonlight-tv\; cd /home/apps/moonlight-tv\; tar zxvf -' < "${CPACK_PACKAGE_FILE_NAME}.tar.gz"
+            COMMAND ssh steamlink 'mkdir -p /home/apps/\; cd /home/apps/\; unzip -' < "${CPACK_PACKAGE_FILE_NAME}.zip"
             WORKING_DIRECTORY ${CPACK_PACKAGE_DIRECTORY}
             DEPENDS steamlink-package-moonlight)
 endif ()
