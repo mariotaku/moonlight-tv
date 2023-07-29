d683320283e73219ecd476e3953bb191ca37caa8
Ningyuan Li <mariotaku.lee@gmail.com>
2023-07-30 00:43:55 +0900

    covert to true color if cover is indexed

--- src/app/ui/launcher/coverloader.c
+++ src/app/ui/launcher/coverloader.c
@@ -294,6 +294,14 @@ static bool coverloader_filecache_get(coverloader_req_t *req) {
         SDL_FreeSurface(decoded);
         return false;
     }
+    if (decoded->format->palette != NULL) {
+        SDL_Surface* true_color = SDL_ConvertSurfaceFormat(decoded, SDL_PIXELFORMAT_RGB24, 0);
+        SDL_FreeSurface(decoded);
+        if (true_color == NULL) {
+            return false;
+        }
+        decoded = true_color;
+    }
     int sw = decoded->w, sh = decoded->h;
     while (sw > req->target_width * 1.5 || sh > req->target_height * 1.5) {
         sw /= 2;
