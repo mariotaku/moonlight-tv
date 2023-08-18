3a419f382ad05f5895ad2c04c7c8df07587ad0cf
Ningyuan Li <mariotaku.lee@gmail.com>
2023-08-18 16:48:18 +0900

    mark ares-package error fatal

--- cmake/AresPackage.cmake
+++ cmake/AresPackage.cmake
@@ -3,10 +3,12 @@ execute_process(COMMAND ares-package "${CPACK_TEMPORARY_DIRECTORY}" -o "${CPACK_
         -e cmake
         -e "libmbedtls[.].*"
         -e "lib/static"
+        COMMAND_ERROR_IS_FATAL ANY
         )
 
 execute_process(COMMAND webosbrew-gen-manifest -p "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_FILE_NAME}.ipk"
         -o "${CPACK_PACKAGE_DIRECTORY}/${CPACK_PACKAGE_NAME}.manifest.json"
         -i "https://github.com/mariotaku/moonlight-tv/raw/main/deploy/webos/icon.png"
         -l "https://github.com/mariotaku/moonlight-tv"
+        COMMAND_ERROR_IS_FATAL ANY
         )
