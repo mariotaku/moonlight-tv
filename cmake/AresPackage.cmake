8b66df34f800354ca95386308a4d8aeaab4ee1c1
Ningyuan Li <mariotaku.lee@gmail.com>
2023-09-16 10:50:48 +0900

    supports x86 webOS emulator

--- cmake/AresPackage.cmake
+++ cmake/AresPackage.cmake
@@ -4,6 +4,7 @@ execute_process(COMMAND ares-package "${CPACK_TEMPORARY_DIRECTORY}" -o "${CPACK_
         -e "libmbedtls[.].*"
         -e "lib/static"
         -e "lib/pkgconfig"
+        --force-arch "${CPACK_WEBOS_PACKAGE_ARCH}"
         COMMAND_ERROR_IS_FATAL ANY
         )
 
