b1529e86802fd295413da0a6fa3e29dd4e899679
Ningyuan Li <mariotaku.lee@gmail.com>
2024-05-01 01:07:34 +0900

    fixed syntax

--- src/app/app.c
+++ src/app/app.c
@@ -73,7 +73,7 @@ int app_init(app_t *app, app_settings_loader *settings_loader, int argc, char *a
         SDL_SetHint(SDL_HINT_WEBOS_ACCESS_POLICY_RIBBON, "false");
     }
 #else
-    if (app.settings.syskey_capture) {
+    if (app->settings.syskey_capture) {
         SDL_SetHint(SDL_HINT_GRAB_KEYBOARD, "1");
     }
 #endif
