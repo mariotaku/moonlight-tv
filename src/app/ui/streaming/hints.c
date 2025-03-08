5c3efb177d077b32d86d071b63c173f1b387367c
Mariotaku <mariotaku.lee@gmail.com>
2025-03-08 19:57:43 +0900

    Updated Translations (#512)

--- src/app/ui/streaming/hints.c
+++ src/app/ui/streaming/hints.c
@@ -5,10 +5,12 @@
 
 #define HINTS_LEN 4
 static const char *hints_list[HINTS_LEN] = {
-        "Long press BACK (or press EXIT for traditional TV remote) while streaming, to open status overlay.",
-        "Frequent lagging on 5GHz Wi-Fi? Try changing to another channel, check Help for details.",
-        "Xbox One/Series controller can't be directly connected via cable or official adapter.",
-        "If you have ultra-wide monitor, you may need to change its resolution to fit 16:9 for streaming.",
+        translatable(
+                "Long press BACK (or press EXIT for traditional TV remote) while streaming, to open status overlay."),
+        translatable("Frequent lagging on 5GHz Wi-Fi? Try changing to another channel, check Help for details."),
+        translatable("Xbox One/Series controller can't be directly connected via cable or official adapter."),
+        translatable(
+                "If you have ultra-wide monitor, you may need to change its resolution to fit 16:9 for streaming."),
 };
 
 const char *hints_obtain() {
