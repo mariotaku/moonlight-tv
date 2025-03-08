e8ddba026795fbe06e151e3eb739503ecfcbdaf3
Ningyuan Li <mariotaku.lee@gmail.com>
2025-03-08 18:26:39 +0900

    updated i18n

--- cmake/MoonlightI18n.cmake
+++ cmake/MoonlightI18n.cmake
@@ -8,10 +8,10 @@ endif ()
 set(MOFILES)
 foreach (LANG ${I18N_LOCALES})
     string(REPLACE "-" "_" _MOLANG ${LANG})
-    set(_POFILE ${CMAKE_SOURCE_DIR}/i18n/${LANG}/messages.po)
+    set(_POFILE ${CMAKE_SOURCE_DIR}/src/i18n/${LANG}/messages.po)
     set(_MODIR ${CMAKE_BINARY_DIR}/mo/${_MOLANG}/LC_MESSAGES)
     set(_MOFILE ${_MODIR}/moonlight-tv.mo)
-    add_custom_command(OUTPUT ${_MOFILE} DEPENDS i18n/${LANG}/messages.po
+    add_custom_command(OUTPUT ${_MOFILE} DEPENDS src/i18n/${LANG}/messages.po
             COMMAND ${CMAKE_COMMAND} -E make_directory ${_MODIR}
             COMMAND ${MSGFMT_COMMAND} --output-file=${_MOFILE} ${_POFILE})
     list(APPEND MOFILES ${_MOFILE})
@@ -21,9 +21,9 @@ add_dependencies(moonlight moonlight-i18n)
 
 file(GLOB_RECURSE I18N_SOURCES LIST_DIRECTORIES FALSE RELATIVE ${CMAKE_SOURCE_DIR} "src/*.c" "src/*.h")
 add_custom_target(i18n-update-pot
-        COMMAND ${XGETTEXT_COMMAND} --keyword=locstr --keyword=translatable --add-comments -o i18n/messages.pot
+        COMMAND ${XGETTEXT_COMMAND} --keyword=locstr --keyword=translatable --add-comments -o src/i18n/messages.pot
         ${I18N_SOURCES} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
 add_custom_target(i18n-merge-po
-        COMMAND ${MSGMERGE_COMMAND} --update i18n/ja/messages.po i18n/messages.pot
+        COMMAND ${MSGMERGE_COMMAND} --update src/i18n/ja/messages.po src/i18n/messages.pot
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
