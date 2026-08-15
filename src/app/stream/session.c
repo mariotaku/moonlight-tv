a17c47347187db9a567e944d010dfadca8097f91
keychain2db <keychain2db@protonmail.com>
2026-08-15 16:49:55 -0400

    Settings: add controller touchpad configuration

--- src/app/stream/session.c
+++ src/app/stream/session.c
@@ -248,6 +248,13 @@ void session_config_init(app_t *app, session_config_t *config, const SERVER_DATA
                          const CONFIGURATION *app_config) {
     config->stream = app_config->stream;
     config->vmouse = app_config->virtual_mouse;
+    config->controller_touchpad_mode = app_config->controller_touchpad_mode;
+    config->controller_touchpad_sensitivity = app_config->controller_touchpad_sensitivity;
+    config->controller_touchpad_press = app_config->controller_touchpad_press;
+    config->controller_touchpad_secondary_click = app_config->controller_touchpad_secondary_click;
+    config->controller_touchpad_tap_to_click = app_config->controller_touchpad_tap_to_click;
+    config->controller_touchpad_two_finger_scroll = app_config->controller_touchpad_two_finger_scroll;
+    config->controller_touchpad_invert_two_finger_scroll = app_config->controller_touchpad_invert_two_finger_scroll;
     config->hardware_mouse = app_config->hardware_mouse;
     config->local_audio = app_config->localaudio;
     config->view_only = app_config->viewonly;
--- src/app/ui/settings/panes/input.pane.c
+++ src/app/ui/settings/panes/input.pane.c
@@ -11,11 +11,25 @@ typedef struct input_pane_t {
     lv_obj_t *absmouse_toggle;
     lv_obj_t *absmouse_hint;
     lv_obj_t *deadzone_label;
+
+    pref_dropdown_int_entry_t controller_touchpad_mode_entries[3];
+    pref_dropdown_int_entry_t controller_touchpad_press_entries[4];
+
+    lv_obj_t *controller_touchpad_sensitivity_label;
+    lv_obj_t *controller_touchpad_mouse_controls[6];
 } input_pane_t;
 
 static lv_obj_t *create_obj(lv_fragment_t *self, lv_obj_t *view);
 
 static void pane_ctor(lv_fragment_t *self, void *args);
+
+static void on_controller_touchpad_mode_changed(lv_event_t *e);
+
+static void on_controller_touchpad_sensitivity_changed(lv_event_t *e);
+
+static void update_controller_touchpad_mouse_options(input_pane_t *pane);
+
+static void update_controller_touchpad_sensitivity_label(input_pane_t *pane);
 
 #if FEATURE_INPUT_EVMOUSE
 static void hwmouse_state_update_cb(lv_event_t *e);
@@ -43,6 +57,22 @@ static lv_obj_t *create_obj(lv_fragment_t *self, lv_obj_t *container) {
     lv_obj_set_layout(view, LV_LAYOUT_FLEX);
     lv_obj_set_flex_flow(view, LV_FLEX_FLOW_COLUMN);
     lv_obj_set_flex_align(view, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+    pane->controller_touchpad_mode_entries[0] = (pref_dropdown_int_entry_t) {
+            locstr("Disabled"), CONTROLLER_TOUCHPAD_MODE_DISABLED, false};
+    pane->controller_touchpad_mode_entries[1] = (pref_dropdown_int_entry_t) {
+            locstr("Mouse"), CONTROLLER_TOUCHPAD_MODE_MOUSE, true};
+    pane->controller_touchpad_mode_entries[2] = (pref_dropdown_int_entry_t) {
+            locstr("Native"), CONTROLLER_TOUCHPAD_MODE_NATIVE, false};
+
+    pane->controller_touchpad_press_entries[0] = (pref_dropdown_int_entry_t) {
+            locstr("Off"), CONTROLLER_TOUCHPAD_PRESS_DISABLED, false};
+    pane->controller_touchpad_press_entries[1] = (pref_dropdown_int_entry_t) {
+            locstr("Left click"), CONTROLLER_TOUCHPAD_PRESS_LEFT, true};
+    pane->controller_touchpad_press_entries[2] = (pref_dropdown_int_entry_t) {
+            locstr("Right click"), CONTROLLER_TOUCHPAD_PRESS_RIGHT, false};
+    pane->controller_touchpad_press_entries[3] = (pref_dropdown_int_entry_t) {
+            locstr("Middle click"), CONTROLLER_TOUCHPAD_PRESS_MIDDLE, false};
+
     pref_checkbox(view, locstr("View-only mode"), &app_configuration->viewonly, false);
     pref_desc_label(view, locstr("Don't send mouse, keyboard or gamepad input to host computer."), false);
 
@@ -82,11 +112,110 @@ static lv_obj_t *create_obj(lv_fragment_t *self, lv_obj_t *container) {
     pref_desc_label(view, locstr("Swap A/B and X/Y gamepad buttons. Useful when you prefer Nintendo-like layouts."),
                     false);
 
+    pref_header(view, locstr("Controller touchpad"));
+
+    pref_title_label(view, locstr("Mode"));
+    lv_obj_t *mode_dropdown = pref_dropdown_int(view,
+                                                pane->controller_touchpad_mode_entries,
+                                                sizeof(pane->controller_touchpad_mode_entries) /
+                                                        sizeof(*pane->controller_touchpad_mode_entries),
+                                                &app_configuration->controller_touchpad_mode,
+                                                NULL);
+    lv_obj_set_width(mode_dropdown, LV_PCT(100));
+    lv_obj_add_event_cb(mode_dropdown, on_controller_touchpad_mode_changed, LV_EVENT_VALUE_CHANGED, pane);
+    pref_desc_label(view, locstr("Disabled ignores controller touchpad input. Mouse uses it as a relative "
+                                 "trackpad. Native forwards controller touch events and the touchpad button "
+                                 "to the host."), false);
+
+    size_t mouse_control = 0;
+    pane->controller_touchpad_sensitivity_label = pref_title_label(view, NULL);
+    lv_obj_t *sensitivity_slider =
+            pane->controller_touchpad_mouse_controls[mouse_control++] =
+                    pref_slider(view, &app_configuration->controller_touchpad_sensitivity,
+                                CONTROLLER_TOUCHPAD_SENSITIVITY_MIN,
+                                CONTROLLER_TOUCHPAD_SENSITIVITY_MAX, 5);
+    lv_obj_set_width(sensitivity_slider, LV_PCT(100));
+    lv_obj_add_event_cb(sensitivity_slider, on_controller_touchpad_sensitivity_changed,
+                        LV_EVENT_VALUE_CHANGED, pane);
+    pref_desc_label(view, locstr("Adjust relative pointer speed in Mouse mode."), false);
+
+    pane->controller_touchpad_mouse_controls[mouse_control++] =
+            pref_checkbox(view, locstr("Tap to click"),
+                          &app_configuration->controller_touchpad_tap_to_click, false);
+    pref_desc_label(view, locstr("Tap once with one finger to send a left click. "
+                                 "Touch and hold briefly to hold left click for dragging."), false);
+
+    pref_title_label(view, locstr("Physical press"));
+    lv_obj_t *press_dropdown = pane->controller_touchpad_mouse_controls[mouse_control++] =
+            pref_dropdown_int(view,
+                              pane->controller_touchpad_press_entries,
+                              sizeof(pane->controller_touchpad_press_entries) /
+                                      sizeof(*pane->controller_touchpad_press_entries),
+                              &app_configuration->controller_touchpad_press,
+                              NULL);
+    lv_obj_set_width(press_dropdown, LV_PCT(100));
+    pref_desc_label(view, locstr("Choose the mouse button sent for a normal physical touchpad press."),
+                    false);
+
+    pref_title_label(view, locstr("Secondary click"));
+    lv_obj_t *secondary_click_dropdown = pane->controller_touchpad_mouse_controls[mouse_control++] =
+            pref_dropdown_int(view,
+                              pane->controller_touchpad_press_entries,
+                              sizeof(pane->controller_touchpad_press_entries) /
+                                      sizeof(*pane->controller_touchpad_press_entries),
+                              &app_configuration->controller_touchpad_secondary_click,
+                              NULL);
+    lv_obj_set_width(secondary_click_dropdown, LV_PCT(100));
+    pref_desc_label(view, locstr("Used for a physical press in the lower-right corner or a two-finger tap."),
+                    false);
+
+    pane->controller_touchpad_mouse_controls[mouse_control++] =
+            pref_checkbox(view, locstr("Two-finger scrolling"),
+                          &app_configuration->controller_touchpad_two_finger_scroll, false);
+    pref_desc_label(view, locstr("Scroll vertically or horizontally by moving two fingers together."),
+                    false);
+
+    pane->controller_touchpad_mouse_controls[mouse_control] =
+            pref_checkbox(view, locstr("Invert two-finger scrolling"),
+                          &app_configuration->controller_touchpad_invert_two_finger_scroll, false);
+    pref_desc_label(view, locstr("Use natural scrolling so content follows your fingers, like on macOS."),
+                    false);
+
+    update_controller_touchpad_sensitivity_label(pane);
+    update_controller_touchpad_mouse_options(pane);
+
 #if FEATURE_INPUT_EVMOUSE
     hwmouse_state_update(pane);
 #endif
     update_deadzone_label(pane);
     return view;
+}
+
+static void on_controller_touchpad_mode_changed(lv_event_t *e) {
+    input_pane_t *pane = lv_event_get_user_data(e);
+    update_controller_touchpad_mouse_options(pane);
+}
+
+static void on_controller_touchpad_sensitivity_changed(lv_event_t *e) {
+    input_pane_t *pane = lv_event_get_user_data(e);
+    update_controller_touchpad_sensitivity_label(pane);
+}
+
+static void update_controller_touchpad_mouse_options(input_pane_t *pane) {
+    bool mouse_mode = app_configuration->controller_touchpad_mode == CONTROLLER_TOUCHPAD_MODE_MOUSE;
+    for (size_t i = 0; i < sizeof(pane->controller_touchpad_mouse_controls) /
+                           sizeof(*pane->controller_touchpad_mouse_controls); ++i) {
+        if (mouse_mode) {
+            lv_obj_clear_state(pane->controller_touchpad_mouse_controls[i], LV_STATE_DISABLED);
+        } else {
+            lv_obj_add_state(pane->controller_touchpad_mouse_controls[i], LV_STATE_DISABLED);
+        }
+    }
+}
+
+static void update_controller_touchpad_sensitivity_label(input_pane_t *pane) {
+    lv_label_set_text_fmt(pane->controller_touchpad_sensitivity_label, "%s - %d%%",
+                          locstr("Sensitivity"), app_configuration->controller_touchpad_sensitivity);
 }
 
 #if FEATURE_INPUT_EVMOUSE
